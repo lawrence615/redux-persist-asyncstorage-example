8714d04082d91f220ae025186e7560084104f8de
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-05-29 16:17:35 +0300

    working on improving the code

--- src/presentation/screens/BooksList.js
+++ src/presentation/screens/BooksList.js
@@ -1,22 +1,111 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SafeAreaView, View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+
+import { getBooks } from '../../application/redux/actions'
 
 
 const BooksList = () => {
+
+  const dispatch = useDispatch()
+  const {books} = useSelector(state => state.booksReducer)
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ marginVertical: 12 }}>
+        <View style={{ flexDirection: 'row', flex: 1 }}>
+          {/* Book Cover */}
+          <Image
+            source={{ uri: item.image_url }}
+            resizeMode='cover'
+            style={{ width: 100, height: 150, borderRadius: 10 }}
+          />
+          {/* Book Metadata */}
+          <View style={{ flex: 1, marginLeft: 12 }}>
+            {/* Book Title */}
+            <View>
+              <Text style={{ fontSize: 22, paddingRight: 16, color: 'white' }}>
+                {item.title}
+              </Text>
+            </View>
+            {/* Meta info */}
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 10,
+                alignItems: 'center'
+              }}
+            >
+              <MaterialCommunityIcons
+                color='#64676D'
+                name='book-open-page-variant'
+                size={20}
+              />
+              <Text style={{ fontSize: 14, paddingLeft: 10, color: '#64676D' }}>
+                {item.num_pages}
+              </Text>
+              <MaterialCommunityIcons
+                color='#64676D'
+                name='star'
+                size={20}
+                style={{ paddingLeft: 16 }}
+              />
+              <Text style={{ fontSize: 14, paddingLeft: 10, color: '#64676D' }}>
+                {item.rating}
+              </Text>
+            </View>
+            {/* Buttons */}
+            <View style={{ marginTop: 14 }}>
+              <TouchableOpacity
+                onPress={() => console.log('Bookmarked!')}
+                activeOpacity={0.7}
+                style={{
+                  flexDirection: 'row',
+                  padding: 2,
+                  backgroundColor: '#2D3038',
+                  borderRadius: 20,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: 40,
+                  width: 40
+                }}
+              >
+                <MaterialCommunityIcons
+                  color='#64676D'
+                  size={24}
+                  name='bookmark-outline'
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const fetchBooks = () => dispatch(getBooks());
+
+  useEffect(() => {
+    // fetchBooks();
+    dispatch(getBooks())
+  },[])
+
   return (
-    <View style={styles.container}>
-      <Text>BooksList</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#1E1B26' }}>
+      <View style={{ flex: 1, paddingHorizontal: 16 }}>
+        <Text style={{ color: 'white', fontSize: 22 }}>Bestsellers</Text>
+        <View style={{ flex: 1, marginTop: 8 }}>
+          <FlatList
+            data={books}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderItem}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
+  </SafeAreaView>
   )
 }
 
 export default BooksList
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
