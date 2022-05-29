412b4279867e70888ab9718a5274435ac011d080
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-05-29 08:55:41 +0300

    added babel aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver', {
+          alias: {
+            navigator: "./src/presentation/navigator",
+            screens: "./src/presentation/screens"
+          }
+        }
+      ]
+    ]
   };
 };
