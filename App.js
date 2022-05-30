9e15569f896eec8248c96372672a8ca05e884d85
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-05-30 09:45:56 +0300

    added redux-persist and linked it with store. And AsyncStorage as the engine used save and persist data

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import {store} from 'store'
+import {store, persistor} from 'store'
 import AppNavigator from 'navigator/AppNavigator';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator/>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppNavigator/>
+      </PersistGate>
     </Provider>
   );
 }
--- src/application/store/index.js
+++ src/application/store/index.js
@@ -1,8 +1,19 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
 
 import booksReducer from "../redux/reducers";
 
-const rootReducer = combineReducers({ booksReducer })
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage, // storage engine used to save and persist the data
+    whitelist: ['bookmarks'] // defines which object key to use from the initial state to save the data
+}
+
+const rootReducer = combineReducers({ 
+    booksReducer: persistReducer(persistConfig, booksReducer)
+ })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const persistor = persistStore(store)
