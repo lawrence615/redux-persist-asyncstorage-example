083bba067a7dc24d93d1c5edacababafaeabee95
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-05-30 09:47:44 +0300

    added action creators used to update the state when the bookmarks are added or removed.

--- src/application/redux/actions.js
+++ src/application/redux/actions.js
@@ -28,3 +28,11 @@ export const getBooks = () => {
         console.log('error:',error);
     }
 }
+
+export const addBookmark = book => dispatch => {
+  dispatch({ type: ADD_TO_BOOKMARK_LIST, payload: book })
+}
+
+export const removeBookmark = book => dispatch => {
+  dispatch({ type: REMOVE_FROM_BOOKMARK_LIST, payload: book })
+}
