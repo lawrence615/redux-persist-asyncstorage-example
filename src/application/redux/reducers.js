684365ead34167d566027851e6708af9406ce1bc
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-05-30 09:48:31 +0300

    added new cases to handle the actions added

--- src/application/redux/reducers.js
+++ src/application/redux/reducers.js
@@ -1,4 +1,4 @@
-import { GET_BOOKS } from "./actions";
+import { GET_BOOKS, ADD_TO_BOOKMARK_LIST, REMOVE_FROM_BOOKMARK_LIST } from "./actions";
 
 const initialState = {
     books: [],
@@ -8,7 +8,11 @@ const initialState = {
 function booksReducer (state = initialState, action){
     switch (action.type) {
         case GET_BOOKS:
-            return { ...state, books: action.payload}    
+            return { ...state, books: action.payload}   
+        case ADD_TO_BOOKMARK_LIST:
+            return { ...state, booksmarks: [...state.booksmarks, action.payload]} 
+        case REMOVE_FROM_BOOKMARK_LIST:
+            return { ...state, booksmarks: state.booksmarks.filter(book => book.id !== action.payload.id)}
         default:
             return state;
     }
