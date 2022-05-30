b53a764231cb57db87dec58bdba64b2ea83b4b28
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-05-30 10:30:06 +0300

    This screen now displays bookmarked books and fetching from local storage

--- src/presentation/screens/BookmarksList.js
+++ src/presentation/screens/BookmarksList.js
@@ -1,11 +1,100 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView, View, Text, FlatList, Image, TouchableOpacity,  StyleSheet } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { removeBookmark } from 'rdx/actions';
 
 const BookmarksList = () => {
+
+  const dispatch = useDispatch()
+  const removeFromBookmarkList = book => dispatch(removeBookmark(book))
+  const {bookmarks} = useSelector(state => state.booksReducer)
+
+  const handleRemoveBookmark = book => {
+    removeFromBookmarkList(book)
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ marginVertical: 12 }}>
+        <View style={{ flexDirection: 'row', flex: 1 }}>
+          {/* Book Cover */}
+          <Image
+            source={{ uri: item.image_url }}
+            resizeMode='cover'
+            style={styles.image}
+          />
+          {/* Book Metadata */}
+          <View style={{ flex: 1, marginLeft: 12 }}>
+            {/* Book Title */}
+            <View>
+              <Text style={styles.bookTitle}>
+                {item.title}
+              </Text>
+            </View>
+            {/* Meta info */}
+            <View
+              style={styles.info}
+            >
+              <MaterialCommunityIcons
+                color='#64676D'
+                name='book-open-page-variant'
+                size={20}
+              />
+              <Text style={styles.pages}>
+                {item.num_pages}
+              </Text>
+              <MaterialCommunityIcons
+                color='#64676D'
+                name='star'
+                size={20}
+                style={{ paddingLeft: 16 }}
+              />
+              <Text style={styles.rating}>
+                {item.rating}
+              </Text>
+            </View>
+            {/* Buttons */}
+            <View style={{ marginTop: 14 }}>
+              <TouchableOpacity
+                onPress={() => handleRemoveBookmark(item)}
+                activeOpacity={0.7}
+                style={styles.bookmarkButton}
+              >
+                <MaterialCommunityIcons
+                  color='#64676D'
+                  size={24}
+                  name='bookmark-remove'
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>BookmarksList</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#1E1B26' }}>
+      <View style={{ flex: 1, paddingHorizontal: 16 }}>
+        <Text style={{ color: 'white', fontSize: 22 }}>Bookmarks</Text>
+        <View style={{ flex: 1, marginTop: 8 }}>
+          {bookmarks.length === 0 ? (
+            <Text style={{ color: '#64676D', fontSize: 18 }}>
+              Add a book to bookmark list.
+            </Text>
+          ) : (
+            <FlatList
+              data={bookmarks}
+              keyExtractor={item => item.id.toString()}
+              renderItem={renderItem}
+              showsVerticalScrollIndicator={false}
+            />
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -17,5 +106,40 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
-      }
+    },
+    image: {
+      width: 100,
+      height: 150, 
+      borderRadius: 10
+    },
+    bookTitle: {
+      fontSize: 22,
+      paddingRight: 16,
+      color: 'white'
+    },
+    info:{
+      flexDirection: 'row',
+      marginTop: 10,
+      alignItems: 'center'
+    },
+    pages: { 
+      fontSize: 14, 
+      paddingLeft: 10, 
+      color: '#64676D' 
+    },
+    rating: {
+      fontSize: 14, 
+      paddingLeft: 10, 
+      color: '#64676D'
+    },
+    bookmarkButton: {
+       flexDirection: 'row',
+       padding: 2,
+       backgroundColor: '#2D3038',
+       borderRadius: 20,
+       alignItems: 'center',
+       justifyContent: 'center',
+       height: 40,
+       width: 40
+    }
 })
